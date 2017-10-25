93d6b4a2051ece7df47f8f9c0b209525bf0b239e
Alex Scott <scotta@bpc.ac.uk>
2017-10-25 14:58:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Write your if/elseif/else statement here!
+    $name = "Terry";
+    if ($name = "Alex") {
+        echo "the condition is true";
+    }
+    else {
+        echo  "the condition is false";
+    }
+      ?>
+    </p>
+  </body>
+</html>
