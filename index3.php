361ee310918559d908fd1579e8a44a87d3c0a1ca
Alex Scott <scotta@bpc.ac.uk>
2017-10-25 15:56:08 +0100

    Create index3.php

---
+++ index3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $fruit = "Apple";
+    
+    switch ($fruit) {
+        case 'Apple':
+            echo "Yummy.";
+            break;
+        default:
+        echo "none of the above";
+    }
+    
+    ?>
+    </body>
+</html>
