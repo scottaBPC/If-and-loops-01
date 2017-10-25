3aa76e8e9722b607d250c6735fb603cf3b482fc9
Alex Scott <scotta@bpc.ac.uk>
2017-10-25 16:10:49 +0100

    Create index5.php

---
+++ index5.php
@@ -0,0 +1,15 @@
+<?php
+     $test = 2;
+        switch ($test) {
+            case 1:
+            echo "good 1";
+            break;
+            case 2:
+            echo "2 good";
+            break;
+            case 3:
+            echo "great work, isn't definitley there somewhere";
+            break;
+            default:
+            echo "what's going on";
+		}
