f5dd98cf10b49fc534bd622a9a243c0eb951e688
Alex Scott <scotta@bpc.ac.uk>
2017-10-25 15:32:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $fName = "alex";
+    $sName = "scott";
+    $fullname = $fName . " " . $sName;
+    
+    echo $fullname;
+    
+    if ($fullname == "alex scott") {
+        echo "holy crap, thats a great name";
+    }
+    elseif
+        {
+            ($fullname == "adam nicholls") {
+            echo "thats kinda cool too";
+        }
+    else
+    {
+        echo "don't like you're name much mate";
+    }    
+    
+    ?>
+    </body>
+</html>
